59821e49a6f43aaef0ce9eff0491a6a9413ef914
princentf07 <felix.pakpahan10@gmail.com>
2025-12-11 15:30:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,29 @@ import streamlit as st
 
 st.title(":orange[Hello, *World!*] :sunglasses:")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "# First, import the elements you need
+
+from streamlit_elements import elements, mui, html
+
+# Create a frame where Elements widgets will be displayed.
+#
+# Elements widgets will not render outside of this frame.
+# Native Streamlit widgets will not render inside this frame.
+#
+# elements() takes a key as parameter.
+# This key can't be reused by another frame or Streamlit widget.
+
+with elements("new_element"):
+
+    # Let's create a Typography element with "Hello world" as children.
+    # The first step is to check Typography's documentation on MUI:
+    # https://mui.com/components/typography/
+    #
+    # Here is how you would write it in React JSX:
+    #
+    # <Typography>
+    #   Hello world
+    # </Typography>
+
+    mui.Typography("Hello world")"
 )
