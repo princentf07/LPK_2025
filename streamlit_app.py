a44197d148d180995dd563d903459be8178500b4
princentf07 <felix.pakpahan10@gmail.com>
2025-12-11 15:32:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,12 @@
-import streamlit as st
 
 st.title(":orange[Hello, *World!*] :sunglasses:")
 st.write(
-    "# First, import the elements you need
+    import streamlit as st
 
-from streamlit_elements import elements, mui, html
+with st.container():
+    st.write("This is inside the container")
 
-# Create a frame where Elements widgets will be displayed.
-#
-# Elements widgets will not render outside of this frame.
-# Native Streamlit widgets will not render inside this frame.
-#
-# elements() takes a key as parameter.
-# This key can't be reused by another frame or Streamlit widget.
+    # You can call any Streamlit command, including custom components:
+    st.bar_chart(np.random.randn(50, 3))
 
-with elements("new_element"):
-
-    # Let's create a Typography element with "Hello world" as children.
-    # The first step is to check Typography's documentation on MUI:
-    # https://mui.com/components/typography/
-    #
-    # Here is how you would write it in React JSX:
-    #
-    # <Typography>
-    #   Hello world
-    # </Typography>
-
-    mui.Typography("Hello world")"
-)
+st.write("This is outside the container")
