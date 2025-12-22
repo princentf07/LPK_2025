0667ac57aedf299879e8a727cffd251c7bb8ec6e
princentf07 <felix.pakpahan10@gmail.com>
2025-12-22 12:30:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,16 @@
 import streamlit as st
 
-with st.container():
-    st.write("This is inside the container")
+st.write("Hello, *WARYOW* :sunglasses:")
 
-    # You can call any Streamlit command, including custom components:
-    st.bar_chart(np.random.randn(50, 3))
 
-st.write("This is outside the container")
+
+
+st.title("This is a title")
+st.title(":blue[PENENTUAN BILANGAN GANJIL GENAP] :sunglasses:")
+st.title(":orange[coba masukan angkamu]")
+
+number = st.number_input("Insert a number", min_value=0, max_value=1000000000000)
+if number%2==1:
+  st.write('bilangan', number, 'termasuk bilangan ganjil')
+else:
+  st.write('bilangan', number, 'termasuk bilangan genap')
